37957f5df69fc5b1fbc3a4cff68c7a11fb49662b
Luiz4us8av6 <luizg8stavo17@gmail.com>
2023-06-25 16:35:39 -0300

    feat: lendo linha de entrada

---
+++ build/CMakeFiles/concordo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/concordo.dir/main.cpp.o"
+  "CMakeFiles/concordo.dir/main.cpp.o.d"
+  "concordo"
+  "concordo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/concordo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
