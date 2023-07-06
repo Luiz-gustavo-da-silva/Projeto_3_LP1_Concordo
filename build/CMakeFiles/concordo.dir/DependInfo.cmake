0125677906376ad95f12c64da500bcec311ba827
Luiz4us8av6 <luizg8stavo17@gmail.com>
2023-07-06 19:51:03 -0300

    feat: criação do metodo salvarUsuarios

---
+++ build/CMakeFiles/concordo.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/luiz/Área de Trabalho/Projeto_3_LP1_Concordo-1/main.cpp" "CMakeFiles/concordo.dir/main.cpp.o" "gcc" "CMakeFiles/concordo.dir/main.cpp.o.d"
+  "/home/luiz/Área de Trabalho/Projeto_3_LP1_Concordo-1/src/servidor.cpp" "CMakeFiles/concordo.dir/src/servidor.cpp.o" "gcc" "CMakeFiles/concordo.dir/src/servidor.cpp.o.d"
+  "/home/luiz/Área de Trabalho/Projeto_3_LP1_Concordo-1/src/sistema.cpp" "CMakeFiles/concordo.dir/src/sistema.cpp.o" "gcc" "CMakeFiles/concordo.dir/src/sistema.cpp.o.d"
+  "/home/luiz/Área de Trabalho/Projeto_3_LP1_Concordo-1/src/usuario.cpp" "CMakeFiles/concordo.dir/src/usuario.cpp.o" "gcc" "CMakeFiles/concordo.dir/src/usuario.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
